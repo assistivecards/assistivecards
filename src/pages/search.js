0cd65541113509a61cc29b7b7cde2a1a72a5b674
Burak Tokak <iletisim@buraktokak.com>
2020-12-09 00:25:14 +0300

    example api search

--- src/pages/search.js
+++ src/pages/search.js
@@ -7,7 +7,6 @@ import T from "../ui"
 import { element } from "prop-types"
 
 class Search extends Component {
-/*
   constructor(props){
     super(props);
     this.state = {
@@ -15,24 +14,19 @@ class Search extends Component {
     }
   }
 
-  UNSAFE_componentWillMount(){
-    this.searchIcons()
+  handleChange(event) {
+    this.searchIcons(event.target.value);
   }
 
-  searchIcons(){
-    fetch('https://api.svgapi.com/v1/Ty5WcDa63E/list/?search=apple')
+  searchIcons(word){
+    fetch('https://api.svgapi.com/v1/Ty5WcDa63E/list/?search='+word)
       .then(res => res.json())
       .then(parsedJSON => parsedJSON.icons)
-      .then(icons => icons.map(
-        i => this.setState({
-          icons: i
-        })
-      ))
-      
-      
-      .catch(err => console.log(err))
+      .then(icons => {
+        this.setState({icons: icons})
+      }).catch(err => console.log(err))
   }
-*/
+
   render(){
     return(
       <Layout language={"en"}>
@@ -40,8 +34,14 @@ class Search extends Component {
         <div className="content">
           <h1>Search</h1>
           <div className="searchContent">
-            <input type="text" className="searchInput" placeholder="Search" /> 
+            <input type="text" className="searchInput" placeholder="Search" onChange={this.handleChange.bind(this)} />
             <button className="searchButton"><img src={require("../images/search.svg")} className="searchimg" /></button>
+          </div>
+
+          <div>
+          {this.state.icons.map((icon, i) => {
+            return <img src={icon.url} style={{width: 50, height: 50}}/>
+          })}
           </div>
         </div>
       </Layout>
