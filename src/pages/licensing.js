2d2f20b3ac4ecfe79b35893df46e41dba0c784ee
Burak Tokak <iletisim@buraktokak.com>
2021-02-15 04:28:04 +0300

    add paddle

--- src/pages/licensing.js
+++ src/pages/licensing.js
@@ -1,17 +1,26 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Link, graphql } from "gatsby"
+import Helmet from "react-helmet"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import T from "../ui"
 
-
 export default function Template({
   data, path // this prop will be injected by the GraphQL query below.
 }) {
 
+  let openCheckout = () => {
+
+    window.Paddle.Setup({ vendor: 106664 });
+    window.Paddle.Checkout.open({ product: 645445 });
+	}
+
   return (
     <Layout language={"en"}>
+      <Helmet>
+        <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+      </Helmet>
       <SEO title="Licensing" description="Helps non-verbal kids to communicate with their parents, teachers and friends."/>
       <div className="content">
         <div className="blogPostCarrier">
@@ -31,7 +40,10 @@ export default function Template({
 
           <p><strong>One-time Payment: $99</strong></p>
           <p>We will also send you all the Illustrations in SVG and PNG format, and JSON files in a single .zip file via <em>Paddle</em>.</p>
-          <p><em style={{color: "red"}}>We will have a buy button here soon!</em></p>
+          <div className="btn" onClick={() => openCheckout()}>Buy a License ($99)</div>
+          <p style={{fontSize: 12}}>Prices may differ depending on your location, currency rates and VAT.<br/>
+          We collect payments using Paddle.com's services with the company name Dream Oriented Ltd<br/>
+          <a href="https://paddle.com/legal-buyers/" target="_blank">Paddle Buyer Terms and Conditions</a></p>
         </div>
       </div>
     </Layout>
