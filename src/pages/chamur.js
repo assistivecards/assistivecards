4aab980fb7e6c54dc5de9487799e1253455713f3
Ebru Kaya <ebru@ebrukaya.me>
2022-11-04 21:49:55 +0100

    description updates

--- src/pages/chamur.js
+++ src/pages/chamur.js
@@ -11,7 +11,7 @@ export default class Chamur extends React.Component {
   render() {
     return (
       <Layout>
-        <SEO title="Chamur" description="Helps non-verbal kids to communicate with their parents, teachers and friends." />
+        <SEO title="Chamur" description="An educational scavenger hunt game that uses object recognition via device camera, and its power." />
 
         <div className="section">
           <div className="sectionDescription right">
--- src/pages/wingo.js
+++ src/pages/wingo.js
@@ -11,13 +11,13 @@ export default class Wingo extends React.Component {
   render() {
     return (
       <Layout>
-        <SEO title="Wingo" description="Helps non-verbal kids to communicate with their parents, teachers and friends." />
+        <SEO title="Wingo" description="Plan your children's day and communicate their tasks better and easier." />
 
         <div className="section">
           <div className="sectionDescription right">
             <img src={require("../images/wingo/wingo.png")} style={{ width: 100, height: 100, marginBottom: 25, boxShadow: "0px 0px 5px 1px rgba(99, 110, 181, 0.4)", borderRadius: "50%" }} />
 
-            <h1 style={{ fontSize: 38 }}>Plan your childs day with<br />
+            <h1 style={{ fontSize: 38 }}>Plan your child's day with<br />
               Wingo App</h1>
 
             <p style={{ fontSize: 23 }}>Plan your children's day and communicate their tasks better and easier.</p>
