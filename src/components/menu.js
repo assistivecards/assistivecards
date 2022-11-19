5d46382699abe9b3c174c36e4ba96226e97e8165
Burak Tokak <iletisim@buraktokak.com>
2022-11-19 14:37:27 +0300

    fix mobile header link pressable area

--- src/components/menu.js
+++ src/components/menu.js
@@ -7,7 +7,7 @@ const Menu = ({ language }) => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <div>
+    <>
       <div className="menuToggler" onClick={() => setMenu(!menu)}>
         <svg version="1.1" width={34} height={34} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
            viewBox="0 0 460.713 460.713">
@@ -33,7 +33,7 @@ const Menu = ({ language }) => {
           </Link>
         }
       </div>
-    </div>
+    </>
   )
 }
 
