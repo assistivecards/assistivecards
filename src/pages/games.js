14c2e64b2649281fefe037f83a294d6b2eee3f56
Burak Tokak <iletisim@buraktokak.com>
2023-02-03 01:14:38 +0300

    add circle game links

--- src/pages/games.js
+++ src/pages/games.js
@@ -16,6 +16,22 @@ export default class Apps extends React.Component {
 					<div className="apps" style={{ width: "100%", margin: "auto" }}>
 
 						<div className="app">
+								<img src={"https://api.assistivecards.com/games/icon/circle@0.5x.png"} style={{ boxShadow: "0px 0px 5px 1px rgba(99, 178, 181, 0.4)", borderRadius: "15%", marginBottom: 10 }} />
+								<div className="appContent" style={{paddingTop: 0}}>
+									<h1>Circle Objects Game</h1>
+									<p style={{marginBottom: 10}}>Circle the right card by drawing on the device, improves kids' motor skills.</p>
+									<div className="download" style={{marginBottom: 0}}>
+										<a href="https://apps.apple.com/us/app/draw-circles-learn-objects/id1668076300" target="_blank">
+											<img src={require("../images/as.svg")} style={{height:40}}/>
+										</a>
+										<a href="https://play.google.com/store/apps/details?id=com.assistivecards.circle" target="_blank">
+											<img src={require("../images/gp.svg")} style={{height:40}}/>
+										</a>
+									</div>
+								</div>
+						</div>
+
+						<div className="app">
 								<img src={"https://api.assistivecards.com/games/icon/silhouette@0.5x.png"} style={{ boxShadow: "0px 0px 5px 1px rgba(105, 137, 255, 0.4)", borderRadius: "15%", marginBottom: 10 }} />
 								<div className="appContent" style={{paddingTop: 0}}>
 									<h1>Silhouette Matching Game</h1>
@@ -37,22 +53,6 @@ export default class Apps extends React.Component {
 									<h1>Card Memory Game</h1>
 									<p style={{marginBottom: 10}}>The goal is to find a pair by flipping face down cards and matching them.</p>
 									<div className="download" style={{marginBottom: 0}}>
-										<a href="https://apps.apple.com/us/app/memorize-card-assistive-game/id1658292272" target="_blank">
-											<img src={require("../images/as.svg")} style={{height:40}}/>
-										</a>
-										<a href="https://play.google.com/store/apps/details?id=com.assistivecards.memory" target="_blank">
-											<img src={require("../images/gp.svg")} style={{height:40}}/>
-										</a>
-									</div>
-								</div>
-						</div>
-
-						<div className="app">
-								<img src={"https://api.assistivecards.com/games/icon/circle@0.5x.png"} style={{ boxShadow: "0px 0px 5px 1px rgba(99, 178, 181, 0.4)", borderRadius: "15%", marginBottom: 10 }} />
-								<div className="appContent" style={{paddingTop: 0}}>
-									<h1>Circle Objects Game</h1>
-									<p style={{marginBottom: 10}}>Circle the right card by drawing on the device, improves kids' motor skills.</p>
-									<div className="download" style={{marginBottom: 0, opacity: 0.1}}>
 										<a href="https://apps.apple.com/us/app/memorize-card-assistive-game/id1658292272" target="_blank">
 											<img src={require("../images/as.svg")} style={{height:40}}/>
 										</a>
